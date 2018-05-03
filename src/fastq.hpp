55810e7baa45c4ca0d0f5521949db9b7b0dc0285
David Seifert <SoapZA@users.noreply.github.com>
2018-05-03 13:19:21 +0200

    Fix loading of old, pre-1.8 Illumina files

--- src/fastq.hpp
+++ src/fastq.hpp
@@ -30,6 +30,7 @@
 #include <vector>
 
 #include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/regex.hpp>
 
 #include "debug.hpp"
 
@@ -47,7 +48,7 @@ template <typename T>
 void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vector<T>& reads, const bool paired_end, const bool second_in_pair) noexcept
 {
 	uint8_t phase = 0;
-	std::size_t line = 0;
+	std::size_t line = 1;
 
 	const char* id_ptr = nullptr;
 	std::size_t id_len = 0;
@@ -58,13 +59,39 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 	const char* qual_ptr = nullptr;
 	std::size_t qual_len = 0;
 
-	bool found_space = false;
 	bool read_second_in_pair = false;
-	char first_letter_after_space = '\0';
 	int header_length = 0;
 	int spacer_length = 0;
 
 	DEBUG_TRACE(std::cerr << std::boolalpha);
+
+	// Identify Illumina headers
+	// https://en.wikipedia.org/wiki/FASTQ_format
+	//
+	// 1.) Pre-1.8 format:
+	//     @HWUSI-EAS100R:6:73:941:1973#0/1
+	//
+	//       HWUSI-EAS100R:6:73:941:1973#0 -> readname
+	//       /1                            -> matepair
+	//
+	// 2.) 1.8+ format:
+	//     @EAS139:136:FC706VJ:2:2104:15343:197393 1:Y:18:ATCACG
+	//
+	//       EAS139:136:FC706VJ:2:2104:15343:197393 -> readname
+	//       1                                      -> matepair
+	//       :Y:18:ATCACG                           -> remaining auxiliary info (discarded)
+
+	boost::regex illumina_old{ "([[:graph:]]*)/([[:digit:]])" };
+	boost::regex illumina_new{ "([[:graph:]]*) ([[:digit:]]):[[:graph:]]*:[[:graph:]]*:[[:graph:]]*" };
+	boost::cmatch header_matches;
+	bool found_match = false;
+
+	enum class illumina_format
+	{
+		OLD,
+		NEW,
+		UNKNOWN
+	} header_format;
 
 	const char* start_line_ptr = input_file.data();
 	const char* const end_ptr = input_file.data() + input_file.size();
@@ -72,67 +99,66 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 	{
 		if (*i == '\n')
 		{
-			++line;
-
 			if (i != start_line_ptr)
 			{
 				switch (phase)
 				{
 					case 0: // header line
-						// @EAS139:136:FC706VJ:2:2104:15343:197393 1:Y:18:ATCACG
-						//                                         ^
-						//                identifies first(1) or second(2) in pair
-						if (start_line_ptr[0] != '@')
+						if (*start_line_ptr != '@')
 						{
-							std::cerr << "ERROR: Malformed FASTQ header line " << input_file << " on line " << line << " does not start with '@'." << std::endl;
+							std::cerr << "ERROR: Malformed FASTQ header: line " << line << " does not start with '@'." << std::endl;
 							exit(EXIT_FAILURE);
 						}
 
 						id_ptr = start_line_ptr + 1;
 						header_length = i - id_ptr;
-						if (found_space == true)
+
+						found_match = ((header_format = illumina_format::NEW, boost::regex_search(id_ptr, i, header_matches, illumina_new)) || (header_format = illumina_format::OLD, boost::regex_search(id_ptr, i, header_matches, illumina_old)) || (header_format = illumina_format::UNKNOWN, false));
+
+						read_second_in_pair = paired_end;
+						if (found_match)
 						{
-							// identifier contains space, can possibly
-							// extract mate-pair information
-							if ((paired_end == true) && (header_length >= static_cast<int>(id_len) + 2))
+							id_ptr = header_matches[1].first;
+							id_len = header_matches[1].second - id_ptr;
+
+							const char mate_pair_identifier = *(header_matches[2].first);
+							switch (mate_pair_identifier)
 							{
-								// has at least one character overhanging
-								first_letter_after_space = *(id_ptr + id_len + 1);
-								read_second_in_pair = (first_letter_after_space == '2');
-							}
-							else
-							{
-								read_second_in_pair = (paired_end == true) && (second_in_pair == true);
+								case '1':
+									read_second_in_pair = false;
+									break;
+								case '2':
+									read_second_in_pair &= true;
+									break;
+								default:
+									std::cerr << "ERROR: Malformed FASTQ header: line " << line << " contained invalid matepair specifier '" << mate_pair_identifier << "'." << std::endl;
+									exit(EXIT_FAILURE);
+									break;
 							}
 						}
 						else
 						{
-							// identifier does not contain space,
-							// hence does not allow for determining mate-pair information
-							id_len = header_length;
-							read_second_in_pair = (paired_end == true) && (second_in_pair == true);
+							id_len = i - id_ptr;
+							read_second_in_pair &= second_in_pair;
 						}
 
-						DEBUG_TRACE(std::cerr << "Full Header:     " << boost::string_ref(start_line_ptr, header_length + 1) << " (len=" << header_length + 1 << ")" << std::endl);
-						DEBUG_TRACE(std::cerr << "Header(I):       " << boost::string_ref(id_ptr, id_len) << " (len=" << id_len << ")" << std::endl);
-						DEBUG_TRACE(std::cerr << "Contains space:  " << found_space << std::endl);
-						DEBUG_TRACE(if (found_space) std::cerr
-							<< "Header(II):      " << boost::string_ref(id_ptr + id_len + 1, header_length - id_len - 1) << " (len=" << header_length - id_len - 1 << ")" << std::endl
-							<< "Letter:          " << (first_letter_after_space ? std::string(1, first_letter_after_space) : std::string("N/A")) << std::endl);
-						DEBUG_TRACE(std::cerr << "Second in pair:  " << read_second_in_pair << std::endl);
+						DEBUG_TRACE(std::cerr << "Full Header:     '" << boost::string_ref(id_ptr, header_length) << "' (len=" << header_length << ")" << std::endl);
+						DEBUG_TRACE(std::cerr << "Format:           " << (header_format == illumina_format::NEW ? "New Illumina 1.8+" : (header_format == illumina_format::OLD ? "Old Illumina <1.8" : "Unknown format")) << std::endl);
+						DEBUG_TRACE(std::cerr << "Readname:        '" << boost::string_ref(id_ptr, id_len) << "' (len=" << id_len << ")" << std::endl);
+						DEBUG_TRACE(std::cerr << "Second in pair:   " << read_second_in_pair << std::endl);
 						break;
 
 					case 1: // DNA sequence
 						seq_ptr = start_line_ptr;
 						seq_len = i - seq_ptr;
 
-						DEBUG_TRACE(std::cerr << "DNA sequence:    " << boost::string_ref(seq_ptr, seq_len) << " (len=" << seq_len << ")" << std::endl);
+						DEBUG_TRACE(std::cerr << "DNA sequence:    '" << boost::string_ref(seq_ptr, seq_len) << "' (len=" << seq_len << ")" << std::endl);
 						break;
 
 					case 2: // '+' with possibly id following again
 						if (start_line_ptr[0] != '+')
 						{
-							std::cerr << "ERROR: Malformed FASTQ spacer line " << input_file << " on line " << line << " does not start with '+'." << std::endl;
+							std::cerr << "ERROR: Malformed FASTQ spacer: line " << line << " does not start with '+'." << std::endl;
 							exit(EXIT_FAILURE);
 						}
 
@@ -140,11 +166,11 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 						if ((spacer_length) && (spacer_length != header_length))
 						{
 							// should contain copy of sequence
-							std::cerr << "ERROR: Malformed FASTQ spacer line " << input_file << " on line " << line << " does not have same length (" << spacer_length << ") as header length (" << header_length << ")." << std::endl;
+							std::cerr << "ERROR: Malformed FASTQ spacer: line " << line << " does not have same length (" << spacer_length << ") as header length (" << header_length << ")." << std::endl;
 							exit(EXIT_FAILURE);
 						}
 
-						DEBUG_TRACE(std::cerr << "Spacer line:     " << boost::string_ref(start_line_ptr + 1, spacer_length) << " (len=" << spacer_length << ")" << std::endl);
+						DEBUG_TRACE(std::cerr << "Spacer line:     '" << boost::string_ref(start_line_ptr + 1, spacer_length) << "' (len=" << spacer_length << ")" << std::endl);
 						break;
 
 					case 3: // Phred scores
@@ -158,12 +184,8 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 						}
 
 						reads.emplace_back(id_ptr, id_len, seq_ptr, seq_len, qual_ptr, qual_len, read_second_in_pair);
-						DEBUG_TRACE(std::cerr << "Phred/Qual line: " << boost::string_ref(qual_ptr, qual_len) << " (len=" << qual_len << ")" << std::endl
+						DEBUG_TRACE(std::cerr << "Phred/Qual line: '" << boost::string_ref(qual_ptr, qual_len) << "' (len=" << qual_len << ")" << std::endl
 											  << std::endl);
-
-						// reset for next read
-						found_space = false;
-						first_letter_after_space = '\0';
 						break;
 				}
 
@@ -172,23 +194,13 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 			}
 
 			start_line_ptr = i + 1;
+			++line;
 		}
 		else
 		{
 			const char cur_letter = *i;
 			switch (phase)
 			{
-				case 0:
-					// somewhere in the middle of the header line
-					if ((cur_letter == ' ') && (found_space == false))
-					{
-						// found the first space in the header line,
-						// need to clip off everything beyond
-						found_space = true;
-						id_len = i - start_line_ptr - 1;
-					}
-					break;
-
 				case 1:
 					switch (cur_letter)
 					{
@@ -204,12 +216,12 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 						case 'g':
 						case 't':
 						case 'n':
-							std::cerr << "ERROR: Malformed FASTQ sequence line " << input_file << " on line " << line << " contains lowercase DNA character '" << cur_letter << "'. DNA characters need to be uppercase." << std::endl;
+							std::cerr << "ERROR: Malformed FASTQ sequence: line " << line << " contains lowercase DNA character '" << cur_letter << "'. DNA characters need to be uppercase." << std::endl;
 							exit(EXIT_FAILURE);
 							break;
 
 						default:
-							std::cerr << "ERROR: Malformed FASTQ sequence line " << input_file << " on line " << line << " contains invalid character '" << cur_letter << "'. Allowed DNA characters are 'A', 'C', 'G', 'T' and 'N'." << std::endl;
+							std::cerr << "ERROR: Malformed FASTQ sequence: line " << line << " contains invalid character '" << cur_letter << "'. Allowed DNA characters are 'A', 'C', 'G', 'T' and 'N'." << std::endl;
 							exit(EXIT_FAILURE);
 							break;
 					}
@@ -218,7 +230,7 @@ void fastq_read(const boost::iostreams::mapped_file_source& input_file, std::vec
 				case 3:
 					if ((cur_letter < 33) || (cur_letter > 126))
 					{
-						std::cerr << "ERROR: Malformed FASTQ quality line " << input_file << " on line " << line << " contains invalid quality character '" << cur_letter << "'. The range has to start at '!' (= 0) and end at '~' (= 93), inclusive." << std::endl;
+						std::cerr << "ERROR: Malformed FASTQ qualities: line " << line << " contains invalid quality character '" << cur_letter << "'. The range has to start at '!' (= 0) and end at '~' (= 93), inclusive." << std::endl;
 						exit(EXIT_FAILURE);
 					}
 
